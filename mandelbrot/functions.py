e7c302fa7a8567a9066a63c443ddc2f82f15ce8d
wsfhahn <wsfhahn@icloud.com>
2025-08-17 15:15:57 -0400

    This is a much nicer way to do it

--- mandelbrot/functions.py
+++ mandelbrot/functions.py
@@ -1,4 +1,5 @@
 from mandelbrot.types import Z
+from math import sqrt
 
 def step(z: Z, c: Z) -> Z:
     # We'll treat the real part of the constant as x and the imaginary part as y
@@ -10,8 +11,15 @@ def step(z: Z, c: Z) -> Z:
     b_0 = z.imag
 
     # Evolve a and evolve b
-    a_1 = a_0**2 - b_0**2 + x
-    b_1 = 2*(a_0*b_0) + y
+    try:
+        a_1 = a_0**2 - b_0**2 + x
+        b_1 = 2*(a_0*b_0) + y
+    # If the number grows too large, just return the input
+    except OverflowError:
+        return Z(
+            real=a_0,
+            imag=b_0
+        )
 
     # Return our complex number
     return Z(
@@ -19,30 +27,45 @@ def step(z: Z, c: Z) -> Z:
         imag=b_1
     )
 
-def iterate(n: int, c: Z) -> list[Z]:
-    # z begins as 0
+def get_magnitude(z: Z) -> float:
+    a = z.real
+    b = z.imag
+
+    # Because a complex number exists on a 2D plane, we can get the magnitude using pythag theorem
+    mag = sqrt(a**2 + b**2)
+    return mag
+
+def in_mandelbrot_set(n: int, c: Z) -> tuple[list[Z], bool]:
+    # Start with z at 0, per the definition
     z = Z(
         real=0,
         imag=0
     )
 
-    # Evolve z with our constant c and save each step in a list
+    # List to store results
     z_set: list[Z] = []
+
+    # Iterate n times, evolving real and imaginary part with each step
     for _ in range(n):
         z_set.append(z)
         z = step(z, c)
+
+        # If the magnitude is greater than 2, we know we've diverged
+        if abs(get_magnitude(z)) >= 2:
+            print("Diverging, not in set!")
+            return (z_set, False)
     
-    # Return the list
-    return z_set
+    # If we get here, it didn't diverge with n iterations
+    print("Did not diverge, in set!")
+    return (z_set, True)
 
 if __name__ == "__main__":
-    n = 5
+    n = 50
     c = Z(
         real=-1,
         imag=0
     )
 
-    results = iterate(n, c)
+    z_set, is_mandelbrot = in_mandelbrot_set(n, c)
 
-    for r in results:
-        print(r)
+    print(is_mandelbrot)
